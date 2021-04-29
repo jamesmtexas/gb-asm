1a269c81ed6b79f12319feab4083b8dea654184e
James Murphy <jamesmtexas@protonmail.com>
2021-04-29 16:10:29 +0000

    First commit

---
+++ main.asm
@@ -0,0 +1,18 @@
+INCLUDE "hardware.inc"
+
+SECTION "Header", ROM0[$100]
+
+EntryPoint:
+	di
+	jp Start
+
+REPT $150 - $104
+	db 0
+ENDR
+
+SECTION "Game code", ROM0
+Start:
+
+
+.lockup
+	jr .lockup
